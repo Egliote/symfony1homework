dccf31445902fd39083d405dae7076ffc11aa4b4
Egliote <egliote@outlook.com>
2018-04-18 11:35:02 +0300

    Code formatting corrected.

--- src/Controller/StudentInfoController.php
+++ src/Controller/StudentInfoController.php
@@ -8,44 +8,40 @@ use Symfony\Component\HttpFoundation\Request;
 
 class StudentInfoController extends Controller
 {
-    private $data_file='data.json';
+    private $data_file = 'data.json';
+
     /**
      * @Route("/student/info", name="student_info")
      */
     public function index()
     {
-        $data=file_get_contents($this->data_file);
-        $teams=json_decode($data);
+        $data = file_get_contents($this->data_file);
+        $teams = json_decode($data);
         $request = Request::createFromGlobals();
         $request->getPathInfo();
         $studentName = $request->query->get('utm_term');
 
-        if ($studentName == "Eglė")
-        {
-            $message="Dešimt balų";
-            $format="table-success";
-        } else
-        {
-            $message="Gal pasiseks kitą kartą";
-            $format="table-warning";
+        if ($studentName == "Eglė") {
+            $message = "Dešimt balų";
+            $format = "table-success";
+        } else {
+            $message = "Gal pasiseks kitą kartą";
+            $format = "table-warning";
         }
 
-        $foundTeam=null;
-        $foundTeamName=null;
-        foreach ($teams as $teamName => $team)
-        {
-            foreach ($team->members as $student)
-            {
-                if ($studentName == $student)
-                {
-                    $foundTeam=$team;
-                    $foundTeamName=$teamName;
+        $foundTeam = null;
+        $foundTeamName = null;
+        foreach ($teams as $teamName => $team) {
+            foreach ($team->members as $student) {
+                if ($studentName == $student) {
+                    $foundTeam = $team;
+                    $foundTeamName = $teamName;
                     break;
                 }
             }
         }
 
-        if ($foundTeam!=null && $foundTeamName!=null) {
+        if ($foundTeam != null && $foundTeamName != null) {
             return $this->render('student_info/index.html.twig', [
                 'team' => $foundTeamName,
                 'mentor' => $foundTeam->mentor,
@@ -53,9 +49,8 @@ class StudentInfoController extends Controller
                 'message' => $message,
                 'format' => $format
             ]);
-        } else
-        {
-            $errorMessage="Student not found";
+        } else {
+            $errorMessage = "Student not found";
             return $this->render('student_not_found/index.html.twig', [
                 'message' => $errorMessage
             ]);
